a15e49218fd68c33b94874839b3c0c24c4074570
LindseyShantanu <ltrahan2013@gmail.com>
2023-02-08 14:14:52 -0600

    help me with my sound file issues

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
 public class App {
@@ -15,7 +19,10 @@ public class App {
 		//shuffle the deck
 		deck1.shuffle();
 		System.out.println("First, we must shuffle.");
-		playMusic(C:\Users\tahmi\Bootcamp\Week6\shuffling-cards-1);
+		//Try to add shuffle sound clip
+
+		
+		playMusic("shuffling-cards-1.mp3");
 		
 		
 		/* Use to print the deck:
@@ -32,18 +39,29 @@ public class App {
 				p2.draw(deck1);
 		}
 		
+		//play War
+		for(int i=0; i<26; i++) {
+			int x = p1.flip().getValue();
+			int y = p2.flip().getValue();
+			
+		}
+		
 
 	}
 	
 	public static void playMusic(String filepath) {
-		InputStream sound;
-		try {
-			sound = new FileInputStream(new File(filepath));
-			AudioStream aud = new AudioStream(sound);
-			AudioPlayer.player.start(aud);
-		} catch(Exception e) {
-			JOptionPane.showMessageDialog(null,"Error");
-		}
+	    InputStream sound;
+	    try {
+	        sound = new FileInputStream(new File(filepath));
+	        BufferedInputStream bis = new BufferedInputStream(sound);
+	        AudioInputStream ais = AudioSystem.getAudioInputStream(bis);
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(ais);
+	        clip.start();
+	    } catch(Exception e) {
+	        JOptionPane.showMessageDialog(null,"Error");
+	    }
 	}
-
+		
 }
+
