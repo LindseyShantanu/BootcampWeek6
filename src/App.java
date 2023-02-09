e69c2cc3fcc14f2573909dd4fe6e0092ab7f4d1c
LindseyShantanu <ltrahan2013@gmail.com>
2023-02-09 11:01:40 -0600

    final commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -19,19 +20,16 @@ public class App {
 		//shuffle the deck
 		deck1.shuffle();
 		System.out.println("First, we must shuffle.");
-		//Try to add shuffle sound clip
 
+			//play a shuffle sound and wait
+		playMusic("shuffling-cards-1.wav");
+		oneSec();
 		
-		playMusic("shuffling-cards-1.mp3");
-		
-		
-		/* Use to print the deck:
-		for (Card card: deck1.getCards()) {
-			card.describe();
-		}*/
 		
 		//deal the deck
 		System.out.println("Let's deal the deck.");
+		playMusic("cards03.wav");
+		oneSec();
 		for(int i=0; i<52; i++) {
 			if (i%2==0)
 				p1.draw(deck1);
@@ -41,12 +39,89 @@ public class App {
 		
 		//play War
 		for(int i=0; i<26; i++) {
-			int x = p1.flip().getValue();
-			int y = p2.flip().getValue();
+			//let's make it a little nicer to look at
+			//This will break up each round
+			for(int j=0; j<50; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			for(int j=0; j<50; j++) {
+				System.out.print("-");
+			}
+			System.out.println();
+			for(int j=0; j<50; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 			
+			//Player 1 turn
+			oneSec();
+			playMusic("page-flip-01a.wav");
+			Card c = p1.flip();
+			int x = c.getValue();
+			System.out.print(p1.name + " plays "); 
+				c.describe();
+			
+			//Player 2 turn
+			oneSec();
+			playMusic("page-flip-02.wav");
+			Card c2 = p2.flip();
+			int y = c2.getValue();
+			System.out.print(p2.name + " plays "); 
+				c2.describe();
+			
+			//Compare scores and give points
+			oneSec();
+			if(x>y) {
+				p1.incrementScore();
+				if(x==y+1 || x==14)
+					playMusic("yes.wav");
+				System.out.println(p1.name + " gets a point and has " + p1.score+" points.");
+			}
+			else if (y>x){
+				p2.incrementScore();
+				if(y==x+1 || y==14)
+					playMusic("haha.wav");
+				System.out.println(p2.name + " gets a point and has " + p2.score+" points.");
+
+			}
+			else {
+				System.out.println("Tie - no points");
+
+			}
+			oneSec();
 		}
 		
-
+		//compare final score
+			//Adding in the fancy break lines
+			for(int j=0; j<50; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			for(int j=0; j<50; j++) {
+				System.out.print("-");
+			}
+			System.out.println();
+			for(int j=0; j<50; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		if (p1.score>p2.score) {
+			System.out.println("\033[1mWINNER! WINNER!\033[0m");
+			System.out.println("Congratulations "+p1.name+"! You win with a score of "
+					+ p1.score+ " to " + p2.score + ".");
+			playMusic("cheerin.wav");
+		} else if (p2.score>p1.score) {
+			System.out.println("\033[1mWINNER! WINNER!\033[0m");
+			System.out.println("Congratulations "+p2.name+"! You win with a score of "
+					+ p2.score+ " to " + p1.score + ".");
+			playMusic("cheerin.wav");
+		} else {
+			System.out.println("\033[1mDraw!\033[0m");
+			System.out.println("It's a tie with a score of "
+					+ p1.score+ " to " + p2.score + ".");
+			playMusic("womp-womp.wav");
+		}
 	}
 	
 	public static void playMusic(String filepath) {
@@ -58,10 +133,26 @@ public class App {
 	        Clip clip = AudioSystem.getClip();
 	        clip.open(ais);
 	        clip.start();
+	        while(clip.getMicrosecondLength()!= clip.getMicrosecondPosition());
 	    } catch(Exception e) {
 	        JOptionPane.showMessageDialog(null,"Error");
 	    }
+	    
 	}
+	
+	public static void oneSec(){
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	/* Use to print the deck:
+	for (Card card: deck1.getCards()) {
+		card.describe();
+	}*/
 		
 }
 
